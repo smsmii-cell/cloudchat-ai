ec93e39f86e67c9c21b3d5f6ad62f31104cbf89f
smsmii-cell <202500069@uachs.org>
2026-03-05 11:51:40 -0500

    Create mockData.js

---
+++ client/src/domains/websites/mockData.js
@@ -0,0 +1,4 @@
+export const sites = [
+  { id: "s1", name: "VendZ Marketplace", description: "A social commerce hub for indie creators.", sections: ["home", "catalog", "community"] },
+  { id: "s2", name: "CloudChat Docs", description: "Product updates and API guides.", sections: ["guides", "api", "changelog"] }
+];
