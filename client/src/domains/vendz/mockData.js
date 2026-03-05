2b55578db6e4f5f829792e2d0d236c914a6d2ad7
smsmii-cell <202500069@uachs.org>
2026-03-05 11:47:49 -0500

    Create mockData.js

---
+++ client/src/domains/vendz/mockData.js
@@ -0,0 +1,4 @@
+export const posts = [
+  { id: "p1", author: "nova", content: "VendZ creators now have profile badges.", likes: 4, comments: 1 },
+  { id: "p2", author: "lyra", content: "Product gallery updated with video previews.", likes: 7, comments: 2 }
+];
